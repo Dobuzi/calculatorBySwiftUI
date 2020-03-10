27b636c7a7994f515dade086ba747db763244ed7
Dobuzi <kimjw7981@me.com>
2020-03-11 04:10:11 +0900

    Initial Commit

---
+++ Cal2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cal2
+//
+//  Created by 김종원 on 2020/03/11.
+//  Copyright © 2020 김종원. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
